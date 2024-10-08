14f58c7d952db7999fe0fa89bf24df395b992818
RVJVIJAY <vijay31235l@gmail.com>
2024-10-08 12:20:42 +0530

    herosection updated

--- src/components/heroSection/HeroSection.tsx
+++ src/components/heroSection/HeroSection.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const HeroSection = () => {
   return (
     <div>
-      <img className='h-40 lg:h-full' src='https://img.notionusercontent.com/s3/prod-files-secure%2Feeee8099-a731-4be4-b949-97588572fb6b%2Faf556a00-6601-4a15-8931-dab16ebd5981%2FUntitled.png/size/w=2000?exp=1727761596&sig=W9gBIq8O0CMBvTLtt5r89HEwm_3aTv1UiPCCE-SZsdI' alt='landingimage'></img>
+      <img className='h-40 lg:h-full' src='/land.png' alt='landingimage'></img>
     </div>
   )
 }
