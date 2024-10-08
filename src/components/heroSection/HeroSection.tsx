b995b97b50ef5b0c373e92758dc54f1dda897abf
VIJAY <148680643+RVJVIJAY@users.noreply.github.com>
2024-10-08 12:25:21 +0530

    update img in herosection

--- src/components/heroSection/HeroSection.tsx
+++ src/components/heroSection/HeroSection.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const HeroSection = () => {
   return (
     <div>
-      <img className='h-40 lg:h-full' src='https://www.shutterstock.com/image-illustration/online-shopping-network-ecommerce-sales-digital-marketing-1524165005' alt='landingimage'></img>
+       <img className='h-40 lg:h-full' src='/land.png' alt='landingimage'></img>
     </div>
   )
 }
